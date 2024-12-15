ebe13ceaa5d736388da4df19161b408ec8aa4f38
watashone <balabas191@gmail.com>
2024-12-15 17:29:30 +0200

    Made task 3

--- task3/index.js
+++ task3/index.js
@@ -1,2 +1,41 @@
 // Task 3
 //   * Integrate AbortController or other Cancellable approach
+
+async function asyncMap(array, callback, signal) {
+    const results = [];
+    for (const element of array) {
+        if (signal.aborted) new Error('Operation was aborted');
+        results.push(await callback(element, signal));
+    }
+    return results;
+}
+
+async function mapCallback(element, signal) {
+    return new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+            resolve(element * 2)
+        }, 1000)
+        signal.addEventListener('abort', () => {
+            clearTimeout(timeout);
+            reject(new Error('Operation was aborted'));
+        })
+    })
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const controller = new AbortController()
+const signal = controller.signal;
+
+(async () => {
+    try {
+        const result = await asyncMap(numbers, mapCallback, signal);
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+})();
+
+setTimeout(() => {controller.abort()}, 2500);
+
+
+
