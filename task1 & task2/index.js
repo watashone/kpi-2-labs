6bd77078972a71fd8e14459f173fa7a1ce19df50
watashone <balabas191@gmail.com>
2024-12-15 17:11:19 +0200

    Made task 1 & 2

--- task1 & task2/index.js
+++ task1 & task2/index.js
@@ -6,3 +6,29 @@
 //   * Prepare promise based alternative
 //   * Write use cases for the promise based solution
 //   * Write use cases for the async-await
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+async function asyncMap(array, callback) {
+    const results = [];
+    for (const element of array) {
+        results.push(await callback(element));
+    }
+    return results;
+}
+
+async function mapCallback(element) {
+    return new Promise(resolve => {
+        resolve(element * 2);
+    })
+}
+
+// promise
+asyncMap(numbers, mapCallback).then(results => console.log(results));
+
+// async & await
+(async () => {
+    const results = await asyncMap(numbers, mapCallback);
+    console.log(results);
+})()
